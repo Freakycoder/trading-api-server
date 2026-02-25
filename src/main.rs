bc33c5e1977ed94fd66743f3e098723bf34d990b
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2026-02-25 16:32:48 +0400

    created shared state including client and registry

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::time::Instant;
+use std::{sync::Arc, time::Instant};
 
 use serde::{Deserialize, Serialize};
 use tokio::net::{TcpListener};
@@ -6,7 +6,7 @@ use tonic::transport::Channel;
 use tonic::Request;
 use axum::{Json, Router, extract::{State}, routing::{get, post}};
 use orderbook_proto::{CancelOrderRequest, CancelOrderResponse, ModifyOrderRequest, ModifyOrderResponse, NewOrderRequest, NewOrderResponse, OrderBookClient, orders::OrderType, BookRequest};
-use prometheus::{HistogramOpts, HistogramVec};
+use prometheus::{HistogramOpts, HistogramVec, IntCounter, Registry};
 use lazy_static::lazy_static;
 
 lazy_static!(
@@ -41,19 +41,35 @@ lazy_static!(
         .buckets(vec![1.0, 5.0, 10.0, 25.0, 50.0, 100.0]), 
         &["asset_name", "status"]
     ).unwrap();
+
+    static ref REQUEST_COUNTER : IntCounter = IntCounter::new("request_counter", "total no of requests").unwrap();
 );
 
+#[derive(Debug, Clone)]
+pub struct SharedState{
+    pub client : OrderBookClient<Channel>,
+    registry : Registry
+}
+
+impl SharedState {
+    pub async fn new() -> Result<Self, anyhow::Error>{
+        let client = OrderBookClient::connect("http://[::1]:50051").await?;
+        let registry = Registry::new();
+        Ok(Self { client, registry })
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let client = OrderBookClient::connect("http://[::1]:50051").await?;
 
+    let shared_state = SharedState::new().await?;
     let app = Router::new()
     .route("/new", post(new_order))
     .route("/modify", post(modify_order))
     .route("/cancel", post(cancel_order))
     .route("/depth", get(depth))
-    .with_state(client);
+    .route("/metric", get(|_| println!("")))
+    .with_state(shared_state);
     
     let listener = TcpListener::bind("127.0.0.1:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
@@ -61,7 +77,7 @@ async fn main() -> Result<(), anyhow::Error> {
 }
 
 async fn new_order(
-    State(mut client) : State<OrderBookClient<Channel>>, // axum clones the client instance for us over here.
+    State(mut shared_state) : State<SharedState>, // axum clones the client instance for us over here.
     Json(request) : Json<NewOrder>, ) -> Json<NewOrderRes>{
         let start_time = Instant::now();
         let req = request;
@@ -74,15 +90,16 @@ async fn new_order(
             order_type : OrderType::Market as i32
         });
         
-        let response = client.new_order(order_request).await.unwrap().into_inner();
+        let response = shared_state.client.new_order(order_request).await.unwrap().into_inner();
         let res_to_send = NewOrderRes::from(response);
         let total_duration = start_time.elapsed().as_millis() as f64;
         NEW_ORDER_TOTAL_DURATION.with_label_values(&["New Order", "success"]).observe(total_duration);
+        REQUEST_COUNTER.inc();
         Json(res_to_send)
 }
 
 async fn modify_order(
-    State(mut client) : State<OrderBookClient<Channel>>, 
+    State(mut shared_state) : State<SharedState>, 
     Json(request) : Json<ModifyOrder>) -> Json<ModifyOrderRes>{
         let start_time = Instant::now();
        
@@ -96,30 +113,32 @@ async fn modify_order(
             new_quantity,
             side : req.is_buy_side
         });
-    let response = client.modify_order(modify_request).await.unwrap().into_inner();
+    let response = shared_state.client.modify_order(modify_request).await.unwrap().into_inner();
     let converted_res = ModifyOrderRes::from(response);
     let total_time = start_time.elapsed().as_millis() as f64;
     MODIFY_ORDER_TOTAL_DURATION.with_label_values(&[converted_res.status.to_string()]).observe(total_time);
+    REQUEST_COUNTER.inc();
     Json(converted_res) 
 
 }
 async fn cancel_order(
-    State(mut client) : State<OrderBookClient<Channel>>,
+    State(mut shared_state) : State<SharedState>,
     Json(request) : Json<CancelOrder>) -> Json<CancelOrderRes> {
         let start_time = Instant::now();
         let req = request;
         let cancel_request = Request::new(CancelOrderRequest{
             order_id : req.order_id
         });
-    let response = client.cancel_order(cancel_request).await.unwrap().into_inner();
+    let response = shared_state.client.cancel_order(cancel_request).await.unwrap().into_inner();
     let converted_res = CancelOrderRes::from(response);
     let total_time = start_time.elapsed().as_millis() as f64;
     CANCEL_ORDER_TOTAL_DURATION.with_label_values(&[converted_res.status.to_string()]).observe(total_time);
+    REQUEST_COUNTER.inc();
     Json(converted_res)
 }
 
 async fn depth(
-    State(mut client) : State<OrderBookClient<Channel>>,
+    State(mut shared_state) : State<SharedState>,
     Json(request) : Json<DepthReq>
 ) -> Json<DepthRes>{
     let start_time = Instant::now();
@@ -134,9 +153,10 @@ async fn depth(
         security_name : req.security_name,
         level_count
     });
-    let response = client.book_depth(depth_request).await.unwrap().into_inner();
+    let response = shared_state.client.book_depth(depth_request).await.unwrap().into_inner();
     let book_depth = response.book_depth;
     let total_time = start_time.elapsed().as_millis() as f64;
+    REQUEST_COUNTER.inc();
     match book_depth{
         Some(book) => {
             DEPTH_TOTAL_DURATION.with_label_values(&[security_name, 200.to_string()]).observe(total_time);
